a068234ccb10398327b52d1cb5a4a5b85cff6f4b
shubham1810 <skd.1810@gmail.com>
2017-01-21 18:02:36 +0530

    Arcs and spirals

--- class_03/main.js
+++ class_03/main.js
@@ -3,7 +3,8 @@ var ctx = canvas.getContext('2d');
 
 var x = canvas.width/2;
 var y = canvas.height/2;
-var radius = 30;
+var radius = 100;
+var scaleFactor = 2.0;
 
 var dx = 1;
 var dy = 1;
@@ -16,17 +17,45 @@ function drawRect(x, y, w, h, color='#ff0000') {
 	ctx.closePath();
 }
 
-function drawCircle(x, y, r, color='#00ff00') {
+function drawCircle(x, y, r, color='#00ff00', fill=true) {
 	ctx.beginPath();
 	ctx.arc(x, y, r, 0, Math.PI*2);
-	ctx.fillStyle = color;
-	ctx.fill();
+	if (fill==true) {
+		ctx.fillStyle = color;
+		ctx.fill();
+	}
+	ctx.strokeStyle = color;
+	ctx.stroke();
 	ctx.closePath();
+}
+
+function randomRange(low=0, high=1) {
+	var range = high - low;
+	return Math.random()*range + low;
 }
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawCircle(x, y, radius);
+	//drawCircle(x, y, 50, color='#0000ff', fill=false);
+
+	var dth = 0.001;
+	var dr = 0.01;
+	var ang = 0.0;
+	var r = 1;
+
+	while (r < 200) {
+		ctx.beginPath();
+		ctx.arc(x, y, r, ang, ang+dth)
+		ctx.strokeStyle = '#0000ff';
+		ctx.stroke();
+		ctx.closePath();
+		ang += dth;
+		r += dr;
+	}
+
+	/*dx = randomRange(-2, 2);
+	dy = randomRange(-2, 2);
+
 	x += dx;
 	y += dy;
 
@@ -35,7 +64,7 @@ function draw() {
 	}
 	if (y + dy < radius || y + dy > canvas.height - radius) {
 		dy = -dy;
-	}
+	}*/
 }
 
 function startSimulation(frameInterval=10) {
