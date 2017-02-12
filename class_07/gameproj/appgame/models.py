38f53bd50d6b709a8c97ccbcc5c5dab5c4d48a1c
shubham1810 <skd.1810@gmail.com>
2017-02-12 18:32:48 +0530

    Code for class 08

--- class_07/gameproj/appgame/models.py
+++ class_07/gameproj/appgame/models.py
@@ -8,8 +8,17 @@ class UserData(models.Model):
 	username = models.CharField(max_length=100, unique=True)
 	user_key = models.CharField(max_length=100)
 
+	def __unicode__(self):
+		return self.username
+
 
 class GameData(models.Model):
 	user = models.ForeignKey(UserData)
 	game_id = models.CharField(max_length=10, null=True)
 	game_score = models.IntegerField(default=0)
+
+	def __unicode__(self):
+		try:
+			return "GameID: " + self.game_id
+		except:
+			return "Empty GameID"
