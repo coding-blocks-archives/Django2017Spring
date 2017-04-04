e82c56cc281deb64b1c37ddad062f5fcf923170b
shubham1810 <skd.1810@gmail.com>
2017-04-04 18:27:24 +0530

    Youtube song downloader bot

--- class_16/djangoCB2017_bot/bot_app/urls.py
+++ class_16/djangoCB2017_bot/bot_app/urls.py
@@ -4,4 +4,5 @@ import views
 urlpatterns = [
     url(r'^$', views.index, name='home'),
     url(r'^353249793:AAHvZltlXyWwvFj5vRDGGPLq4518npidt7s/', views.hook, name='hook'),
+    url(r'^download/', views.download, name='download')
 ]
--- class_16/djangoCB2017_bot/bot_app/views.py
+++ class_16/djangoCB2017_bot/bot_app/views.py
@@ -7,6 +7,8 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from my_bot import sendMessage
 import requests
+from wsgiref.util import FileWrapper
+import os
 
 # https://core.telegram.org/bots/api
 # https://api.telegram.org/bot<token>/METHOD_NAME
@@ -26,9 +28,36 @@ def hook(request):
 	msg = message['text']
 	chat_id = message['chat']['id']
 
-	sendMessage(str(chat_id), get_gif(msg))
+	sendMessage(str(chat_id), get_video(msg))
 
 	return HttpResponse('')
+
+
+def get_video(msg):
+	import time
+	import subprocess
+	os.system('youtube-dl ' + msg + ' -x --audio-format mp3')
+	time.sleep(10)
+	fname = ''
+	l = os.listdir('.')
+	for lx in l:
+		if '.mp3' in lx:
+			fname = lx
+			break
+	return 'https://80e57d9b.ngrok.io/download?name=' + fname
+
+def download(request):
+	name = request.GET.get('name')
+	l = os.listdir('.')
+	print l
+	for lx in l:
+		if '.mp3' in lx:
+			name = lx
+			break
+	song = FileWrapper(open(name, 'rb'))
+	resp =  HttpResponse(song, content_type='audio/mp3')
+	resp['Content-Length'] = os.path.getsize(name)
+	return resp
 
 def get_gif(msg):
 	url = 'http://api.giphy.com/v1/gifs/search?q=' + msg +'&api_key=dc6zaTOxFJmzC'
